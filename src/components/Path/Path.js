1e9653d4a31b098fda79f6a582981b837962edb4
Rubayet-billah <rubayetbillah909@gmail.com>
2022-10-11 21:04:44 +0600

    active link styled

--- src/components/Path/Path.js
+++ src/components/Path/Path.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Path = ({ route }) => {
     const { name, path } = route;
+
+    //active link styles
+    let activeClassName = " border-b-4";
     return (
         <div className='mx-auto py-1 hover:font-bold md:mx-5'>
             {/* <a href={path}>{name}</a> */}
-            <Link to={path}>{name}</Link>
+            {/* <Link to={path}>{name}</Link> */}
+            <NavLink to={path} className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+            }>{name}</NavLink>
         </div>
     );
 };
