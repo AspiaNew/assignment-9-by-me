e5d3c02b2b8953281a285ec88d1aaea4f30a56ea
Rubayet-billah <rubayetbillah909@gmail.com>
2022-10-12 14:11:20 +0600

    link hover effect edited

--- src/components/Path/Path.js
+++ src/components/Path/Path.js
@@ -7,7 +7,7 @@ const Path = ({ route }) => {
     //active link styles
     let activeClassName = "border-b-4";
     return (
-        <div className='mx-auto py-1 hover:font-bold md:mx-5'>
+        <div className='mx-auto py-1 hover:bg-green-500 duration-300 ease-in-out px-3 py-1 rounded md:mx-3'>
             {/* <a href={path}>{name}</a> */}
             {/* <Link to={path}>{name}</Link> */}
             <NavLink to={path} className={({ isActive }) =>
