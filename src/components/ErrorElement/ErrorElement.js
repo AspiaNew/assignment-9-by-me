b88dfbe11d41dd1179575597b82e88849bbe00f5
Rubayet-billah <rubayetbillah909@gmail.com>
2022-10-12 14:47:16 +0600

    text colored

--- src/components/ErrorElement/ErrorElement.js
+++ src/components/ErrorElement/ErrorElement.js
@@ -6,7 +6,7 @@ const ErrorElement = () => {
     console.log(error)
     return (
         <div className='text-center pt-12 md:pt-32 mx-8'>
-            <h1 className='text-3xl font-bold'>Oops!</h1>
+            <h1 className='text-3xl font-bold text-red-500'>Oops!</h1>
             <p className='text-2xl my-3'>Sorry, the page you are visiting is not available at the moment. <br />
                 Please try again next time</p>
             <p>
